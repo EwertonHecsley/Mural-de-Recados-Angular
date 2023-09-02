229b0ce977400c92e2b81c75bba8f073acc29c36
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-09-02 20:21:33 -0300

    criando o backend

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,23 +8,6 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamentoComponent {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Aprendendo a desenvolver em Angular',
-      autoria: 'Ewerton',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Aprendendo a desenvolver em Angular',
-      autoria: 'Ewerton',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: "Em uma noite estrelada, a lua brilhava no céu escuro. O vento sussurrava segredos nas árvores, enquanto o riacho murmurava canções antigas. No silêncio da noite, sonhos ganhavam vida, dançando entre as estrelas, criando um universo de possibilidades.",
-      autoria: 'Ewerton',
-      modelo: 'modelo1'
-    },
-
-  ];
+  listaPensamentos: Pensamento[] = [];
 
 }
